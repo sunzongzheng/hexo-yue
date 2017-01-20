d0c917ffdf0a81aa30deb68a4bc58b52352b5458
sunzongzheng <sunzongzhengwy@163.com>
2017-01-20 21:15:14 +0800

    add nav toggle;移动端适配;分页修改;

--- source/js/hexo-yue.js
+++ source/js/hexo-yue.js
@@ -1,1 +1,9 @@
+//代码高亮
 hljs.initHighlightingOnLoad();
+//nav切换
+var inner_main = document.querySelector('#nav .main');
+var toggle = document.querySelector('#nav .toggle');
+toggle.onclick = function () {
+    inner_main.classList.toggle("disabled");
+    //inner.style.height = inner.offsetHeight;
+};
